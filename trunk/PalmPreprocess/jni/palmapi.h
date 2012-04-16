aa3a32244841e2f3976a6642964d76ae4f678966
achang.zhou@gmail.com <achang.zhou@gmail.com@485d5213-dc66-b1d5-d62d-6d4c9e398cc4>
2012-04-16 08:03:04 +0000

    Prepare for enhence processing.

--- trunk/PalmPreprocess/jni/palmapi.h
+++ trunk/PalmPreprocess/jni/palmapi.h
@@ -7,4 +7,6 @@ void PrepareEnhence(int wid, int hei);
 int LabelCentralArea(unsigned char *nv21_frame, int wid, int hei);
 int LabelPalmArea(unsigned char *nv21_frame, int wid, int hei);
 
+int EnhencePalm(unsigned char *palmMap, int wid, int hei, unsigned char *gray_frame, int wid2, int hei2);
+
 #endif
