731952f91a870e02c3f3ff7d534a809ac18d9a71
achang.zhou <achang.zhou@gmail.com>
2012-04-17 02:17:40 +0000

    Support preview only mode.

--- PalmPreprocess/src/teaonly/projects/palmapi/NativeAPI.java
+++ PalmPreprocess/src/teaonly/projects/palmapi/NativeAPI.java
@@ -5,8 +5,8 @@ import java.net.*;
 import android.util.Log;
 
 class NativeAPI {
-    public static native int nativePrepare(int preWid, int preHei, int procWid, int procHei);
-    public static native int nativeLabelPalm(byte[] frame, int width, int height, Object bmp);
+    public static native int nativePrepare(int preWid, int preHei, int scale);
+    public static native int nativeLabelPalm(byte[] srcFrame, byte[] dstFrame, Object bmp);
 
     public static void LoadLibraries() {
         System.loadLibrary("palmapi");
