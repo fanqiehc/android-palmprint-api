6abb16b8566c5f811d67d03a29b768c406498ff2
achang.zhou <achang.zhou@gmail.com>
2012-04-17 03:25:15 +0000

    Clean code and prepare for enhence processing.

--- PalmPreprocess/jni/palmapi.cpp
+++ PalmPreprocess/jni/palmapi.cpp
@@ -72,5 +72,48 @@ release:
 }
 
 JNIEXPORT void JNICALL JNIDEFINE(nativeEnhencePalm)(JNIEnv* env, jclass clz, jbyteArray map, jbyteArray frame, jobject bmp) {
+     jboolean b;    
 
+    jbyte* mapPtr = env->GetByteArrayElements(map, &b);
+    jbyte* framePtr = env->GetByteArrayElements(frame, &b);
+
+    EnhencePalm((unsigned char *)mapPtr, (unsigned char *)framePtr, labelScale);
+
+#if 0
+    // convert result to bitmap
+	AndroidBitmapInfo  info;
+	unsigned int *pixels;
+    int wid = picWid / labelScale;
+    int hei = picHei / labelScale;
+ 
+	if ((AndroidBitmap_getInfo(env, bmp, &info)) < 0) {  
+    	goto release;
+    } 
+    if ((AndroidBitmap_lockPixels(env, bmp, (void **)&pixels)) < 0) { 
+        goto release;
+    }
+
+   for(int i = 0; i < hei; i++) {
+    	for ( int j = 0; j < wid; j++) {
+            if ( destPtr[j+i*wid] == 1){
+                
+                int y = (int)(1.0 * info.height / wid * j);
+                int x = (int)(1.0 * info.width / hei * (hei - i));
+                
+                for(int m = x-1; m < x+1; m++) 
+                for(int n = y-1; n < y+1; n++) {
+                    if ( m > 0 && n > 0 && m < (int)info.width && n < (int)info.height) {
+    		            unsigned int* rgba = pixels + m + n*info.stride/4;
+                        *rgba = 0xFF00FFFF;
+                    }
+                }
+            }
+        }	
+    }
+    
+    AndroidBitmap_unlockPixels(env, bmp);
+release:    
+#endif
+	env->ReleaseByteArrayElements(frame, framePtr, 0);   
+	env->ReleaseByteArrayElements(map, mapPtr, 0);   
 }
--- PalmPreprocess/src/teaonly/projects/palmapi/NativeAPI.java
+++ PalmPreprocess/src/teaonly/projects/palmapi/NativeAPI.java
@@ -7,6 +7,7 @@ import android.util.Log;
 class NativeAPI {
     public static native int nativePrepare(int preWid, int preHei, int scale);
     public static native int nativeLabelPalm(byte[] srcFrame, byte[] dstFrame, Object bmp);
+    public static native int nativeEnhencePalm(byte[] map, byte[] frame, Object bmp);
 
     public static void LoadLibraries() {
         System.loadLibrary("palmapi");
