6c1ef8c09db51cdf6852cc7b4d0b556903e890d7
achang.zhou@gmail.com <achang.zhou@gmail.com@485d5213-dc66-b1d5-d62d-6d4c9e398cc4>
2012-04-22 11:05:30 +0000

    Clean code, improve label and mark.

--- trunk/PalmReading/jni/palmapi.cpp
+++ trunk/PalmReading/jni/palmapi.cpp
@@ -13,7 +13,7 @@ static int picWid, picHei, labelScale;
 extern "C" {
     JNIEXPORT void JNICALL JNIDEFINE(nativePrepare)(JNIEnv* env, jclass clz, jint picWid, jint picHei, jint scale);
     JNIEXPORT void JNICALL JNIDEFINE(nativeLabelPalm)(JNIEnv* env, jclass clz, jbyteArray src, jbyteArray dst, jobject bmp);
-    JNIEXPORT void JNICALL JNIDEFINE(nativeEnhencePalm)(JNIEnv* env, jclass clz, jbyteArray map, jbyteArray frame, jobject bmp);
+    JNIEXPORT void JNICALL JNIDEFINE(nativeReadingPalm)(JNIEnv* env, jclass clz, jbyteArray map, jbyteArray frame, jobject bmp);
 };
 
 JNIEXPORT void JNICALL JNIDEFINE(nativePrepare)(JNIEnv* env, jclass clz, jint wid, jint hei, jint scale) {
@@ -39,15 +39,47 @@ JNIEXPORT void JNICALL JNIDEFINE(nativeLabelPalm)(JNIEnv* env, jclass clz, jbyte
 	unsigned int *pixels;
     int wid = picWid / labelScale;
     int hei = picHei / labelScale;
- 
+     // marker the central area.
+    int ltx = wid*2/5;
+    int lty = hei/4;
+    int rbx = wid*2/5 + wid*2/5;
+    int rby = hei/4 + hei/2;
+
 	if ((AndroidBitmap_getInfo(env, bmp, &info)) < 0) {  
     	goto release;
     } 
     if ((AndroidBitmap_lockPixels(env, bmp, (void **)&pixels)) < 0) { 
         goto release;
     }
+ 
+    for(int j = ltx; j <= rbx; j++) {
+        int i = lty;
+        int y = (int)(1.0 * info.height / wid * j);
+        int x = (int)(1.0 * info.width / hei * (hei - i));
+        unsigned int* rgba = pixels + x + y*info.stride/4;
+        *rgba = 0xFFFFFFFF;
 
-   for(int i = 0; i < hei; i++) {
+        i = rby;
+        y = (int)(1.0 * info.height / wid * j);
+        x = (int)(1.0 * info.width / hei * (hei - i));
+        rgba = pixels + x + y*info.stride/4;
+        *rgba = 0xFFFFFFFF;
+    }
+    for(int i = lty; i <= rby; i++) {
+        int j = ltx;
+        int y = (int)(1.0 * info.height / wid * j);
+        int x = (int)(1.0 * info.width / hei * (hei - i));
+        unsigned int* rgba = pixels + x + y*info.stride/4;
+        *rgba = 0xFFFFFFFF;
+
+        j = rbx;
+        y = (int)(1.0 * info.height / wid * j);
+        x = (int)(1.0 * info.width / hei * (hei - i));
+        rgba = pixels + x + y*info.stride/4;
+        *rgba = 0xFFFFFFFF;
+    }
+
+    for(int i = 0; i < hei; i++) {
     	for ( int j = 0; j < wid; j++) {
             if ( destPtr[j+i*wid] == 1){
                 
@@ -61,8 +93,9 @@ JNIEXPORT void JNICALL JNIDEFINE(nativeLabelPalm)(JNIEnv* env, jclass clz, jbyte
                         *rgba = 0xFF00FFFF;
                     }
                 }
-            }
-            if ( destPtr[j+i*wid] == 3)
+            } 
+            
+            if ( destPtr[j+i*wid] > 0)
                 destPtr[j+i*wid] = 1;
             else 
                 destPtr[j+i*wid] = 0;            
@@ -76,7 +109,7 @@ release:
 
 }
 
-JNIEXPORT void JNICALL JNIDEFINE(nativeEnhencePalm)(JNIEnv* env, jclass clz, jbyteArray map, jbyteArray frame, jobject bmp) {
+JNIEXPORT void JNICALL JNIDEFINE(nativeReadingPalm)(JNIEnv* env, jclass clz, jbyteArray map, jbyteArray frame, jobject bmp) {
      jboolean b;    
 
     jbyte* mapPtr = env->GetByteArrayElements(map, &b);
